1f5c58ae537ee250df9ba8fe3fd0f4a2ea84cc6b
juan251189 <juan2511@hotmail.com>
2020-09-22 18:36:56 +1000

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,43 @@
   <div id="app">
     <app-quote>
       <h2 slot="title">{{quoteTitle}}</h2>
-      <span slot="subtitle">Some Subtitle</span>
+
       <p slot="content">A wonderful quote</p>
       <button @click="quoteTitle='A wonderful Quote'">change quote</button>
     </app-quote>
+
+    <hr>
+    <div class="components-list">
+
+        <h1>Dynamic components</h1>
+        <button @click="selectedComponent='appHappy'">Happyness</button>
+        <button @click="selectedComponent='appEarth'">Earth</button>
+        <button>Spiritual</button>
+        <button>Motivation</button>
+        <component :is="selectedComponent">
+      </component >
+
+    </div>
+
   </div>
 </template>
 
 <script>
 
-import Quote from './components/Quote.vue'
+import Quote from './components/Quote.vue';
+import Happy from './components/Happy.vue';
+import Earth from './components/Earth.vue';
 export default {
   name: 'App',
   data(){
     return{
-    quoteTitle:'Erase the word dont'
+    quoteTitle:'Erase the word dont',
+    selectedComponent:'appHappy'
 }  },
   components: {
-    appQuote:Quote
+    appQuote:Quote,
+    appHappy:Happy,
+    appEarth:Earth
   }
 }
 </script>
@@ -32,5 +51,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.components-list button{
+  margin: 0 10px;
 }
 </style>
---
+++ src/components/Earth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="happy">
+    <div>
+       <h1>earth</h1>
+        <span style="color:#ccc;" slot="subtitle">Some Subtitle</span>
+    </div>
+    <hr>
+
+  </div>
+</template>
+<script>
+export default {
+
+  data: () => ({
+
+  }),
+  props:['quoteTitle']
+}
+</script>
+<style  scoped>
+    div{
+      border:1px solid #ccc;
+      box-shadow: 1px 1px 2px black;
+      padding: 30px;
+      margin:auto;
+    }
+    h2{
+      color:red;
+    }
+</style>
